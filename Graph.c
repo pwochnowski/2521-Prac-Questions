8ec7c2ca94fa4a13898214d7da0357295feb8c82
pwoch <wochnowskip@gmail.com>
2017-10-13 21:01:15 +1100

    idk

--- Graph.c
+++ Graph.c
@@ -1,18 +1,125 @@
-// Graph.c ... implementation of Graph ADT
-// Written by John Shepherd, May 2013
-
 #include <stdlib.h>
 #include <stdio.h>
-#include <assert.h>
 #include <string.h>
+#include <assert.h>
 #include "Graph.h"
 
-// graph representation (adjacency matrix)
+
+// Practice graph questions for Week 12 Prac Exam
+// Written by Paul Wochnowski and Brittany Evat
+
+/* --------------- README --------------
+ * A quick note before you begin these questions,
+ * it's very strongly recommended that you work these out on pen and paper
+ * before actually coding, figure out what edge cases there are, and just
+ * work through some of those logically on paper.
+ * These questions (tree questions) in particular are typical interview questions.
+ * If you're into role playing (or not), try and practice speaking out your thoughts
+ * the whole time as you're doing these questions on paper/coding. This is what
+ * the interviewer wants to see, your thinking process. It'll also help you maintain
+ * focus and pick up errors which you would otherwise miss.
+ *
+ * https://en.wikipedia.org/wiki/Rubber_duck_debugging is pretty legit
+ *
+ * glhf
+ */
+
+
+// graph representation for you to play around with (adjacency matrix)
 typedef struct GraphRep {
-	int    nV;    // #vertices
-	int    nE;    // #edges
-	int  **edges; // matrix of weights (0 == no edge)
+   int   nV;    // #vertices
+   int   nE;    // #edges
+   int **edges; // matrix of booleans
 } GraphRep;
+
+/* 
+ * Easy Questions 
+ */
+
+// Write a function which takes in a Graph g, and returns 1 if
+// the graph has a cycle, 0 otherwise
+
+int hasCycle(Graph g) {
+
+  return 0;
+}
+
+// Write a function which takes in 
+// g : a Graph
+// s : a starting Vertex
+// d : maximum distance from the starting Vertex
+// vs : the address of an array of Vertex values
+// and stores in vs all vertices which lie within a distance d of the
+// start vertex, and returns a count of the number of Vertex values
+// stored in the array.
+// (Distance is measured in number of edges on the shortest path
+// from the starting vertex)
+
+int within(Graph g, Vertex s, int d, Vertex *vs) {
+
+  return 0;
+}
+
+
+
+/*
+ * Medium Questions
+ */
+
+// Write a function which takes in a Graph g and returns a 
+// vertex-indexed connected components array
+// e.g. a graph with the following adjacency matrix representation 
+// 0 1 0 0
+// 1 0 0 0
+// 0 0 0 1
+// 0 0 1 0
+// would return the following array:
+// [0, 0, 1, 1]
+// i.e. vertices 0 and 1 are in the first connected component (represented by 0 in the array), and
+// vertices 2 and 3 are in the second connected component (represented by 1)
+
+int *createComponents(Graph g) {
+
+  return NULL;
+}
+
+
+/* 
+  Create a bipartition of the graph.
+
+  Determine whether it is possible to split the vertices of a graph into two lists L1 and L2, such that no pair of vertices in the same list are connected by an edge in the graph. 
+  If this is possible, return 1 and store the two partitions seperately in either L1 or L2. 
+  If it is not possible, return 0.
+
+  For example consider the following graph:
+
+  1  -> 2  -> 5
+              ^
+  |           |
+  v           v
+  4 <-> 3  -> 6
+  bipartation(G, l1, l2) == 1,
+  one example of a bipartition for G is L1 = [1, 5, 3], L2 = [2, 4, 6]
+
+
+  1 <-> 2 <-> 3 <---\
+        ^           |
+        |           |
+        v           v 
+        4 <-> 6 <-> 7 
+  bipartition(G, l1, l2) == 0
+
+*/
+
+int bipartition(Graph g, List l1, List l2) {
+
+  return 0;
+}
+
+
+/*
+   You can ignore these methods below, unless you want examples of using the  graph 
+*/
 
 // check validity of Vertex
 int validV(Graph g, Vertex v)
